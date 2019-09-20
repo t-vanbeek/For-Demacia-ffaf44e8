72c5bc228ba23528ded4f8a11b1072f576bedba2
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-09-20 12:48:16 +0200

    Uitwerking van de exercise

--- for_klas.php
+++ for_klas.php
@@ -1,10 +1,12 @@
 <?php
-$klasgenoten = array( 'Thorvald',
-                     'Tyrese',
-                     'Wahid',
-                     'Randy'
+$klasgenoten = array(1 =>'Thorvald',
+                     2 =>'Tyrese',
+                     3=>'Wahid',
+                     4=>'Randy',
+                     5=>'Sven'
                     );
     
+var_dump(count($klasgenoten));
 foreach ($klasgenoten as $klasgenoot){
     echo "$klasgenoot".PHP_EOL;
 }
